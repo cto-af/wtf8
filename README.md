dd979bc43974cd9ed1c839af3d40f211891050dc
Joe Hildebrand <joe-github@cursive.net>
2025-04-27 08:33:24 -0600

    Initial checkin

---
+++ README.md
@@ -0,0 +1,22 @@
+# @cto.af/wtf8
+
+Description
+
+## Installation
+
+```sh
+npm install @cto.af/wtf8
+```
+
+## API
+
+Full [API documentation](http://cto-af.github.io/wtf8/) is available.
+
+Example:
+
+```js
+```
+
+---
+[![Build Status](https://github.com/cto-af/wtf8/workflows/Tests/badge.svg)](https://github.com/cto-af/wtf8/actions?query=workflow%3ATests)
+[![codecov](https://codecov.io/gh/cto-af/wtf8/branch/main/graph/badge.svg?token=N7B7YLIDM4)](https://codecov.io/gh/cto-af/wtf8)
---
+++ typedoc.config.cjs
@@ -0,0 +1,20 @@
+'use strict';
+
+/** @type {import('typedoc').TypeDocOptions} */
+module.exports = {
+  entryPoints: ['src/index.ts'],
+  out: 'docs',
+  cleanOutputDir: true,
+  sidebarLinks: {
+    GitHub: 'https://github.com/cto-af/wtf8/',
+    Documentation: 'http://cto-af.github.io/wtf8/',
+  },
+  navigation: {
+    includeCategories: false,
+    includeGroups: false,
+  },
+  includeVersion: true,
+  categorizeByGroup: false,
+  sort: ['static-first', 'alphabetical'],
+  exclude: ['**/*.spec.ts'],
+};
